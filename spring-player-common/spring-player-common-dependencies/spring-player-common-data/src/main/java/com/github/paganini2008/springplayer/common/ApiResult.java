6bfe4315acd70fd11882247df0dc11454ee510b9
Fred Feng <paganini.fy@gmail.com>
2022-05-20 14:08:55 +0800

    commit

--- spring-player-common/spring-player-common-dependencies/spring-player-common-data/src/main/java/com/github/paganini2008/springplayer/common/ApiResult.java
+++ spring-player-common/spring-player-common-dependencies/spring-player-common-data/src/main/java/com/github/paganini2008/springplayer/common/ApiResult.java
@@ -1,5 +1,7 @@
 package com.github.paganini2008.springplayer.common;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,14 +12,20 @@ import lombok.Setter;
  * @author Fred Feng
  * @version 1.0.0
  */
+@ApiModel("统一响应对象")
 @Getter
 @Setter
 public class ApiResult<T> {
 
+	@ApiModelProperty("成功标识，1:成功 0:失败")
 	private int code;
+	@ApiModelProperty("消息提示")
 	private String msg;
+	@ApiModelProperty("返回值")
 	private T data;
+	@ApiModelProperty("响应耗时")
 	private long elapsed;
+	@ApiModelProperty("接口路径")
 	private String requestPath;
 
 	public static <T> ApiResult<T> ok() {
