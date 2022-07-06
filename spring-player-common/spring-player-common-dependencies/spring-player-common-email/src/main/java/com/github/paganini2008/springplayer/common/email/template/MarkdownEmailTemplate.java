dabb760975804cec4fe5aedec41ac5f202d2b08d
Fred Feng <paganini.fy@gmail.com>
2022-07-06 15:45:25 +0800

    commit

--- spring-player-common/spring-player-common-dependencies/spring-player-common-email/src/main/java/com/github/paganini2008/springplayer/common/email/template/MarkdownEmailTemplate.java
+++ spring-player-common/spring-player-common-dependencies/spring-player-common-email/src/main/java/com/github/paganini2008/springplayer/common/email/template/MarkdownEmailTemplate.java
@@ -1,14 +1,10 @@
 package com.github.paganini2008.springplayer.common.email.template;
 
-import java.io.File;
-import java.util.Collections;
 import java.util.Map;
 
 import org.pegdown.PegDownProcessor;
 
-import com.github.paganini2008.devtools.CharsetUtils;
 import com.github.paganini2008.devtools.StringUtils;
-import com.github.paganini2008.devtools.io.FileUtils;
 
 /**
  * 
@@ -25,12 +21,6 @@ public class MarkdownEmailTemplate implements EmailTemplate {
 		PegDownProcessor pdp = new PegDownProcessor(Integer.MAX_VALUE);
 		String htmlContent = pdp.markdownToHtml(filledTemplateContent);
 		return htmlContent;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		String template = FileUtils.readFileToString(new File("d:/work/云路基础能力.md"), CharsetUtils.UTF_8);
-		MarkdownEmailTemplate emailTemplate = new MarkdownEmailTemplate();
-		System.out.println(emailTemplate.loadContent("云路基础能力", template, Collections.singletonMap("author", "冯岩")));
 	}
 
 }
